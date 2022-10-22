575577da97f883aef8f47c32b461701035c0ebd9
ruriona19 <roberto.uriona19@gmail.com>
2022-10-22 14:17:55 -0400

    Adding comments for sentences and test_sentences.

--- week06/turingTest/sentences.py
+++ week06/turingTest/sentences.py
@@ -1,24 +1,54 @@
+"""
+File: sentences.py
+Author: Roberto Uriona
+Purpose: Generate sentences with four parts:
+a determiner
+a noun
+a verb
+a prepositional phrase
+"""
+
 import random
 
 def main():
-    sentence = get_sentence(1,'past')
+    sentence = make_sentence(1,'past')
     print(f'Single-Past sentence: {sentence}')
 
-    sentence = get_sentence(1,'present')
+    sentence = make_sentence(1,'present')
     print(f'Single-Present sentence: {sentence}')
 
-    sentence = get_sentence(1,'future')
+    sentence = make_sentence(1,'future')
     print(f'Single-Future sentence: {sentence}')
 
-    sentence = get_sentence(2,'past')
+    sentence = make_sentence(2,'past')
     print(f'Plural-Past sentence: {sentence}')
 
-    sentence = get_sentence(2,'present')
+    sentence = make_sentence(2,'present')
     print(f'Plural-Present sentence: {sentence}')
 
-    sentence = get_sentence(2,'future')
+    sentence = make_sentence(2,'future')
     print(f'Plural-Future sentence: {sentence}')
-    
+
+    print('=============================Exceeding the Requirements================================')
+
+    sentence = make_sentence_with_two_prepositional_phrase(1,'past')
+    print(f'Single-Past sentence: {sentence}')
+
+    sentence = make_sentence_with_two_prepositional_phrase(1,'present')
+    print(f'Single-Present sentence: {sentence}')
+
+    sentence = make_sentence_with_two_prepositional_phrase(1,'future')
+    print(f'Single-Future sentence: {sentence}')
+
+    sentence = make_sentence_with_two_prepositional_phrase(2,'past')
+    print(f'Plural-Past sentence: {sentence}')
+
+    sentence = make_sentence_with_two_prepositional_phrase(2,'present')
+    print(f'Plural-Present sentence: {sentence}')
+
+    sentence = make_sentence_with_two_prepositional_phrase(2,'future')
+    print(f'Plural-Future sentence: {sentence}')    
+
 
 def get_determiner(quantity):
     """Return a randomly chosen determiner. A determiner is
@@ -61,9 +91,11 @@ def get_noun(quantity):
     Return: a randomly chosen noun.
     """
     if quantity == 1:
-        nouns = ['bird', 'boy', 'car', 'cat', 'child', 'dog', 'girl', 'man', 'rabbit', 'woman']
+        nouns = ['bird', 'boy', 'car', 'cat', 'child', 'dog', 'girl',
+                 'man', 'rabbit', 'woman']
     else:
-        nouns = ['birds', 'boys', 'cars', 'cats', 'children', 'dogs', 'girls', 'men', 'rabbits', 'women']
+        nouns = ['birds', 'boys', 'cars', 'cats', 'children', 'dogs',
+                 'girls', 'men', 'rabbits', 'women']
 
     # Randomly choose and return a determiner.
     noun = random.choice(nouns)
@@ -96,13 +128,17 @@ def get_verb(quantity, tense):
     Return: a randomly chosen verb.
     """
     if tense == 'past':
-        verbs = ['drank', 'ate', 'grew', 'laughed', 'thought', 'ran', 'slept', 'talked', 'walked', 'wrote']
+        verbs = ['drank', 'ate', 'grew', 'laughed', 'thought', 'ran',
+                 'slept', 'talked', 'walked', 'wrote']
     elif tense == 'present' and quantity == 1:
-        verbs = ['drinks', 'eats', 'grows', 'laughs', 'thinks', 'runs', 'sleeps', 'talks', 'walks', 'writes']
+        verbs = ['drinks', 'eats', 'grows', 'laughs', 'thinks', 'runs',
+                 'sleeps', 'talks', 'walks', 'writes']
     elif tense == 'present' and quantity != 1:
-        verbs = ['drink', 'eat', 'grow', 'laugh', 'think', 'run', 'sleep', 'talk', 'walk', 'write']
+        verbs = ['drink', 'eat', 'grow', 'laugh', 'think', 'run', 'sleep',
+                 'talk', 'walk', 'write']
     elif tense == 'future':
-        verbs = ['will drink', 'will eat', 'will grow', 'will laugh', 'will think', 'will run', 'will sleep', 'will talk', 'will walk', 'will write']
+        verbs = ['will drink', 'will eat', 'will grow', 'will laugh',
+                 'will think', 'will run', 'will sleep', 'will talk', 'will walk', 'will write']
 
     # Randomly choose and return a determiner.
     verb = random.choice(verbs)
@@ -144,22 +180,39 @@ def get_prepositional_phrase(quantity):
     prepositional_phrase = get_preposition() + ' ' + get_determiner(quantity) + ' ' + get_noun(quantity) 
     return prepositional_phrase
 
-def get_sentence(quantity, verb_tense):
-    """Return a randomly chosen determiner. A determiner is
-    a word like "the", "a", "one", "some", "many".
-    If quantity == 1, this function will return either "a",
-    "one", or "the". Otherwise this function will return
-    either "some", "many", or "the".
+def make_sentence(quantity, verb_tense):
+    """Calls get_determiner, get_noun, get_verb, and get_prepositional_phrase functions and 
+       builds and return a sentence.
 
     Parameter
         quantity: an integer.
-            If quantity == 1, this function will return a
-            determiner for a single noun. Otherwise this
-            function will return a determiner for a plural
-            noun.
-    Return: a randomly chosen determiner.
+            If quantity == 1 means that the function in which this parameter will be used, 
+            should return a single determiner or single noun or single present verb otherwise
+            it will return a plural determiner or plural noun.
+        verb_tense: a string that determines the verb conjugation,
+            either "past", "present" or "future"..
+    Return: A sentence that contains a determiner, a noun, a verb, and a prepositional phrase.
     """
-    sentence = get_determiner(quantity).capitalize() + ' ' + get_noun(quantity) + ' ' + get_verb(quantity, verb_tense) + ' ' + get_prepositional_phrase(quantity)
+    sentence = get_determiner(quantity).capitalize() + ' ' + get_noun(quantity) + ' ' + \
+               get_verb(quantity, verb_tense) + ' ' + get_prepositional_phrase(quantity)
+    return sentence
+
+def make_sentence_with_two_prepositional_phrase(quantity, verb_tense):
+    """Calls get_determiner, get_noun, get_prepositional_phrase, get_verb, and 
+       get_prepositional_phrase functions and builds and return a sentence.
+
+    Parameter
+        quantity: an integer.
+            If quantity == 1 means that the function in which this parameter will be used,
+            should return a single determiner or single noun or single present verb otherwise 
+            it will return a plural determiner or plural noun.
+        verb_tense: a string that determines the verb conjugation,
+            either "past", "present" or "future"..
+    Return: A sentence that contains a determiner, a noun, a prepositional phrase, 
+    a verb, and a prepositional phrase.
+    """
+    sentence = get_determiner(quantity).capitalize() + ' ' + get_noun(quantity) + ' ' + \
+               get_prepositional_phrase(1) + ' ' + get_verb(quantity, verb_tense) + ' ' + get_prepositional_phrase(quantity)    
     return sentence
 
 if __name__ == "__main__":
--- week06/turingTest/test_sentences.py
+++ week06/turingTest/test_sentences.py
@@ -1,7 +1,15 @@
-import sentences 
-import random
-import pytest
+"""
+File: test_sentences.py
+Author: Roberto Uriona
+Purpose: Test the functions that generate sentences with four parts:
+a determiner
+a noun
+a verb
+a prepositional phrase
+"""
 
+import sentences 
+import pytest
 
 def test_get_determiner():
     # 1. Test the single determiners.
@@ -9,9 +17,6 @@ def test_get_determiner():
     single_determiners = ['a', 'one', 'the']
 
     # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
     for _ in range(4):
 
         # Call the get_determiner function which
@@ -43,18 +48,15 @@ def test_get_noun():
     single_nouns = ['bird', 'boy', 'car', 'cat', 'child', 'dog', 'girl', 'man', 'rabbit', 'woman']
 
     # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
     for _ in range(4):
 
         # Call the get_noun function which
-        # should return a single determiner.
-        single_noun = sentences.get_noun(1)
+        # should return a single noun.
+        word = sentences.get_noun(1)
 
-        # Verify that the single nount returned from get_determiner
+        # Verify that the single noun returned from get_noun
         # is one of the nouns in the single_nouns list.
-        assert single_noun in single_nouns
+        assert word in single_nouns
 
     # 2. Test the plural nouns.
 
@@ -65,31 +67,27 @@ def test_get_noun():
 
         # Call the get_nouns function which
         # should return a plural noun.
-        plural_noun = sentences.get_noun(2)
+        word = sentences.get_noun(2)
 
         # Verify that the noun returned from get_noun
         # is one of the nouns in the plural_nouns list.
-        assert plural_noun in plural_nouns
+        assert word in plural_nouns
 
 def test_get_verb():
     
-    # 1. Test past verb tense.
+    # 1. Test past tense verbs.
 
     past_tense_verbs = ['drank', 'ate', 'grew', 'laughed', 'thought', 'ran', 'slept', 'talked', 'walked', 'wrote']
 
-    # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
+    # This loop will repeat the statements inside it 4 times.    
     for _ in range(4):
 
-        # Call the get_noun function which
-        # should return a single determiner.
-        verb = sentences.get_verb(1, 'past')
+        # Call the get_verb function which should return a verb in the past tense.
+        word = sentences.get_verb(1, 'past')
 
-        # Verify that the single nount returned from get_determiner
-        # is one of the nouns in the single_nouns list.
-        assert verb in past_tense_verbs
+        # Verify that the verb returned from get_verb
+        # is one of the verbs in the past tense verbs list.
+        assert word in past_tense_verbs
 
     # 2. Test single present verb tense.
 
@@ -99,13 +97,12 @@ def test_get_verb():
     # This loop will repeat the statements inside it 4 times.
     for _ in range(4):
 
-        # Call the get_nouns function which
-        # should return a plural noun.
-        verb = sentences.get_verb(1, 'present')
+        # Call the get_verb function which should return a verb in the sinple present tense.
+        word = sentences.get_verb(1, 'present')
 
-        # Verify that the noun returned from get_noun
-        # is one of the nouns in the plural_nouns list.
-        assert verb in simple_present_tense_verbs
+        # Verify that the verb returned from get_verb
+        # is one of the verbs in the simple present tense verbs list.
+        assert word in simple_present_tense_verbs
 
     # 3. Test plural present verb tense.
 
@@ -114,13 +111,12 @@ def test_get_verb():
     # This loop will repeat the statements inside it 4 times.
     for _ in range(4):
 
-        # Call the get_nouns function which
-        # should return a plural noun.
-        verb = sentences.get_verb(2, 'present')
+        # Call the get_verb function which should return a verb in the plural present tense.
+        word = sentences.get_verb(2, 'present')
 
-        # Verify that the noun returned from get_noun
-        # is one of the nouns in the plural_nouns list.
-        assert verb in plural_present_tense_verbs
+        # Verify that the verb returned from get_verb
+        # is one of the verbs in the plural present tense verbs list.
+        assert word in plural_present_tense_verbs
 
     # 4. Test future verb tense.
 
@@ -129,13 +125,12 @@ def test_get_verb():
     # This loop will repeat the statements inside it 4 times.
     for _ in range(4):
 
-        # Call the get_nouns function which
-        # should return a plural noun.
-        verb = sentences.get_verb(1, 'future')
+        # Call the get_verb function which should return a verb in the future tense.
+        word = sentences.get_verb(1, 'future')
 
-        # Verify that the noun returned from get_noun
-        # is one of the nouns in the plural_nouns list.
-        assert verb in future_tense_verbs
+        # Verify that the verb returned from get_verb
+        # is one of the verbs in the future tense verbs list.
+        assert word in future_tense_verbs
 
 def test_get_preposition():
     # 1. Test the prepositions.
@@ -147,20 +142,72 @@ def test_get_preposition():
         'off', 'on', 'onto', 'out', 'over',
         'past', 'to', 'under', 'with', 'without']
 
-    # This loop will repeat the statements inside it 4 times.
-    # If a loop's counting variable is not used inside the
-    # body of the loop, many programmers will use underscore
-    # (_) as the variable name for the counting variable.
+    # This loop will repeat the statements inside it 4 times.    
     for _ in range(4):
 
-        # Call the get_noun function which
-        # should return a single determiner.
-        preposition = sentences.get_preposition()
+        # Call the get_preposition function which should return a preposition.
+        word = sentences.get_preposition()
 
-        # Verify that the single nount returned from get_determiner
-        # is one of the nouns in the single_nouns list.
+        # Verify that the verb returned from get_preposition
+        # is one of the prepositions in the prepositions list.
+        assert word in prepositions
+
+def test_get_prepositional_phrase():    
+    prepositions = ['about', 'above', 'across', 'after', 'along',
+        'around', 'at', 'before', 'behind', 'below','beyond', 'by', 
+        'despite', 'except', 'for', 'from', 'in', 'into', 'near', 'of',
+        'off', 'on', 'onto', 'out', 'over', 'past', 'to', 'under', 
+        'with', 'without']    
+    single_determiners = ['a', 'one', 'the']
+    plural_determiners = ['some', 'many', 'the']
+    single_nouns = ['bird', 'boy', 'car', 'cat', 'child', 'dog',
+        'girl', 'man', 'rabbit', 'woman']
+    plural_nouns = ['birds', 'boys', 'cars', 'cats', 'children',
+        'dogs', 'girls', 'men', 'rabbits', 'women']
+
+    # 1. Test the single prepositional phrase.
+    # This loop will repeat the statements inside it 4 times.
+    for _ in range(4):
+
+        # Call the get_prepositional_phrase function which
+        # should return a single prepositional phrase.
+        single_prepositional_phrase = sentences.get_prepositional_phrase(1)
+        # Call split function to save each word belonging to preposition_phrase 
+        # in the words list. 
+        words = single_prepositional_phrase.split(' ')
+        preposition = words[0]
+        determiner = words[1]
+        noun = words[2]
+
+        # Test the number of words contained in words list.
+        assert len(words) == 3                        
+        # Test the preposition is in presitions list.
         assert preposition in prepositions
+        # Test the determiner is in single_determiners list.
+        assert determiner in single_determiners
+        # Test the noun is in single_nouns list.
+        assert noun in single_nouns
 
+    # 2. Test the plural prepositional phrase.
+    # This loop will repeat the statements inside it 4 times.
+    for _ in range(4):
+
+        # Call the get_prepositional_phrase function which
+        # should return a plural prepositional phrase.
+        plural_prepositional_phrase = sentences.get_prepositional_phrase(2)
+        words = plural_prepositional_phrase.split(' ')
+        preposition = words[0]
+        determiner = words[1]
+        noun = words[2]
+        
+        # Test the number of words contained in words list.
+        assert len(words) == 3        
+        # Test the preposition is in presitions list.
+        assert preposition in prepositions
+        # Test the determiner is in plural_determiners list.
+        assert determiner in plural_determiners
+        # Test the noun is in plural_nouns list.
+        assert noun in plural_nouns
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
