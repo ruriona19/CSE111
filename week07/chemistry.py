0ca38d8f8c90bd358d05079db37f01085f326233
Roberto Uriona <roberto.uriona@jalasoft.com>
2022-11-04 22:23:19 -0400

    adding Exceeding the Requirements

--- week07/chemistry.py
+++ week07/chemistry.py
@@ -15,6 +15,26 @@ class FormulaError(ValueError):
     """
 
 def main():
+    # Exceeding the Requirements
+    # Add a dictionary that contains known chemical formulas and their names.
+    known_molecules_dict = {
+        "Al2O3": "aluminum oxide",
+        "CH3OH": "methanol",
+        "C2H6O": "ethanol",
+        "C2H5OH": "ethanol",
+        "C3H8O": "isopropyl alcohol",
+        "C3H8": "propane",
+        "C4H10": "butane",
+        "C6H6": "benzene",
+        "C6H14": "hexane",
+        "C8H18": "octane",
+        "CH3(CH2)6CH3": "octane",
+        "C13H18O2": "ibuprofen",
+        "C13H16N2O2": "melatonin",
+        "Fe2O3": "iron oxide",
+        "FeS2": "iron pyrite",
+        "H2O": "water"
+    }
     
     # Get a chemical formula for a molecule from the user.
     molecular_formula = input("Enter the molecular formula of the sample:")
@@ -43,6 +63,13 @@ def main():
     print(f'{molar_mass:.5f} grams/mole')
     # Print the number of moles.
     print(f'{number_of_moles:.5f} moles')
+
+    print()
+    formula = input("Enter a chemical formula to verify if it is present in know_molecules_dict:")
+    # Exceeding the Requirements
+    # Call the get_formula_name function from your main function and print
+    # the compound name for the user to see with the other output.
+    get_formula_name(formula, known_molecules_dict)
 
 def make_periodic_table():         
     filename = 'table_of_elements.json'
@@ -189,6 +216,25 @@ def compute_molar_mass(symbol_quantity_list, periodic_table_dict):
         atomic_mass = float(periodic_table_dict[symbol][ATOMIC_MASS_INDEX])
         total_molar_mass += atomic_mass* quantity
     return total_molar_mass
+
+
+def get_formula_name(formula, known_molecules_dict):
+    """Try to find formula in the known_molecules_dict.
+    If formula is in the known_molecules_dict, return
+    the name of the chemical formula; otherwise return
+    "unknown compound".
+
+    Parameters
+        formula is a string that contains a chemical formula
+        known_molecules_dict is a dictionary that contains
+            known chemical formulas and their names
+    Return: the name of a chemical formula
+    """
+
+    if known_molecules_dict.get(formula) is not None:
+        print(f'Name of the chemical formula: {known_molecules_dict[formula]}')
+    else:
+        print (f'Unknown compound')
 
 
 
