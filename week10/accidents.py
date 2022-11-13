5fb59ce7b0bf3feaa1d2ce412e1d328773cf20a9
Roberto Uriona <roberto.uriona@jalasoft.com>
2022-11-12 23:19:03 -0400

    Week 10

---
+++ week10/accidents.py
@@ -0,0 +1,118 @@
+# Import the csv module so that it can be used
+# to read from the accidents.csv file.
+import csv
+
+
+# Column numbers from the accidents.csv file.
+YEAR_COLUMN = 0
+FATALITIES_COLUMN = 1
+INJURIES_COLUMN = 2
+CRASHES_COLUMN = 3
+FATAL_CRASHES_COLUMN = 4
+DISTRACT_COLUMN = 5
+PHONE_COLUMN = 6
+SPEED_COLUMN = 7
+DUI_COLUMN = 8
+FATIGUE_COLUMN = 9
+
+
+def main():
+    try:
+        # Prompt the user for a filename and open that text file.
+        filename = input("Name of file that contains NHTSA data: ")
+        with open(filename, "rt") as text_file:
+
+            # Prompt the user for a percentage.
+            while True:
+                perc_reduc = float(input("Percent reduction of texting while driving [0, 100]: "))
+                if perc_reduc >= 0 and perc_reduc <= 100:
+                    break
+                else:  
+                    if perc_reduc < 0:
+                        print(f'Error: {perc_reduc} is too low.')
+                    else:
+                        print(f'Error: {perc_reduc} is too high')
+                    print('Please enter a different number.')
+                    continue
+
+            print()
+            print(f"With a {perc_reduc}% reduction in using a cell",
+                "phone while driving, approximately this",
+                "number of injuries and deaths would have",
+                "been prevented in the USA.", sep="\n")
+            print()
+            print("Year, Injuries, Deaths")
+
+            # Use the csv module to create a reader
+            # object that will read from the opened file.
+            reader = csv.reader(text_file)
+
+            # The first line of the CSV file contains column headings
+            # and not a student's I-Number and name, so this statement
+            # skips the first line of the CSV file.
+            next(reader)
+
+            # Process each row in the CSV file.
+            for row in reader:
+                year = row[YEAR_COLUMN]
+
+                # Call the estimate_reduction function.
+                injur, fatal = estimate_reduction(
+                        row, PHONE_COLUMN, perc_reduc)
+
+                # Print the estimated reductions
+                # in injuries and fatalities.
+                print(year, injur, fatal, sep=", ")
+
+    except FileNotFoundError as not_found_err:
+        print(f"{not_found_err}")
+        print("dentiaccs.vscPlese choose a different file.")
+
+    except PermissionError as perm_err:
+        print(f"Error: cannot read from {filename}"
+                " because you don't have permission.")
+
+    except ValueError as val_err:
+        print(f"Error: {val_err}")
+
+
+
+def estimate_reduction(row, behavior_key, perc_reduc):
+    """Estimate and return the number of injuries and deaths that
+    would not have occurred on U.S. roads and highways if drivers
+    had reduced a dangerous behavior by a given percentage.
+
+    Parameters
+        row: a CSV row of data from the U.S. National Highway Traffic
+            Safety Administration (NHTSA)
+        behavior_key: heading from the CSV file for the dangerous
+            behavior that drivers could reduce
+        perc_reduc: percent that drivers could reduce a dangerous
+            behavior
+    Return: The number of injuries and deaths that may have been
+        prevented
+    """
+    try:
+        behavior = int(row[behavior_key])
+        fatal_crashes = int(row[FATAL_CRASHES_COLUMN])
+        ratio = perc_reduc / 100 * behavior / fatal_crashes
+
+        fatalities = int(row[FATALITIES_COLUMN])
+        injuries = int(row[INJURIES_COLUMN])
+
+        reduc_fatal = int(round(fatalities * ratio, 0))
+        reduc_injur = int(round(injuries * ratio, 0))
+        return reduc_injur, reduc_fatal
+    except ZeroDivisionError as zero_div_err:
+        print(f"Error: {zero_div_err}")
+        print("There is a 0 value from the fatal chashes column.")
+        print("Please run the program again and enter an integer")
+        print("larger than 0 for the number of employees.")
+
+
+# If this file was executed like this:
+# > python accidents.py
+# then call the main function. However, if this file
+# was simply imported, then skip the call to main.
+if __name__ == "__main__":
+    main()
---
+++ week10/prove_assignment/receipt.py
@@ -0,0 +1,123 @@
+# Import the csv module so that it can be used
+# to read from the accidents.csv file.
+import csv
+from datetime import datetime
+
+
+# Column numbers from the products.csv file.
+PRODUCT_NUMBER_COLUMN = 0
+PRODUCT_NAME_COLUMN = 1
+PRODUCT_PRICE_COLUMN = 2
+
+
+# Column numbers from the request.csv file.
+REQUEST_PRODUCT_NUMBER_COLUMN = 0
+QUANTITY_COLUMN = 1
+
+
+def main():
+
+    sales_tax_rate = 0.06 
+    number_of_items = 0
+    sub_total = 0.0
+    sales_tax = 0
+    current_date_and_time = datetime.now()
+
+    # Prompt the user for a filename and open that text file.
+    products_dict = read_dict('products.csv', PRODUCT_NUMBER_COLUMN)
+    
+    print('Inkom Emporium')
+    print(products_dict)
+
+    with open("request.csv", "rt") as request_file:
+
+        reader = csv.reader(request_file)
+        next(reader)
+
+        print()
+        print('Requested Items')
+
+        # Read each row in the CSV file one at a time.
+        # The reader object returns each row as a list.
+        for row_list in reader:
+
+            # For the current row, retrieve the
+            # values in columns 0, 3, and 4.
+            product_number_key = row_list[REQUEST_PRODUCT_NUMBER_COLUMN]
+            request_quantity = int(row_list[QUANTITY_COLUMN])
+            product_details = products_dict[product_number_key]
+            product_name = product_details[PRODUCT_NAME_COLUMN]
+            product_price = float(product_details[PRODUCT_PRICE_COLUMN])
+
+            number_of_items += request_quantity
+            sub_total += product_price*request_quantity
+
+            print(f'{product_name}: {request_quantity} @ {product_price}')
+        
+    sales_tax = sub_total*sales_tax_rate
+    total = sales_tax + sub_total
+
+    print()
+    print(f'Number of Items: {number_of_items}')
+    print(f'Subtotal: {sub_total:.2f}')
+    print(f'Sales Tax: {sales_tax:.2f}')
+    print(f'Total: {total:.2f}')   
+    print()
+    print('Thank you for shopping at the Inkom Emporium')   
+    print(f'{current_date_and_time:%a %b %d %I:%M:%S %Y}') 
+
+
+def read_dict(filename, key_column_index):
+    """Read the contents of a CSV file into a compound
+    dictionary and return the dictionary.
+
+    Parameters
+        filename: the name of the CSV file to read.
+        key_column_index: the index of the column
+            to use as the keys in the dictionary.
+    Return: a compound dictionary that contains
+        the contents of the CSV file.
+    """
+    # Create an empty dictionary that will
+    # store the data from the CSV file.
+    dictionary = {}
+
+    # Open the CSV file for reading and store a reference
+    # to the opened file in a variable named csv_file.
+    with open(filename, "rt") as csv_file:
+
+        # Use the csv module to create a reader object
+        # that will read from the opened CSV file.
+        reader = csv.reader(csv_file)
+
+        # The first row of the CSV file contains column
+        # headings and not data, so this statement skips
+        # the first row of the CSV file.
+        next(reader)
+
+        # Read the rows in the CSV file one row at a time.
+        # The reader object returns each row as a list.
+        for row_list in reader:
+
+            # If the current row is not blank, add the
+            # data from the current to the dictionary.
+            if len(row_list) != 0:
+
+                # From the current row, retrieve the data
+                # from the column that contains the key.
+                key = row_list[key_column_index]
+
+                # Store the data from the current
+                # row into the dictionary.
+                dictionary[key] = row_list
+
+    # Return the dictionary.
+    return dictionary
+
+# If this file was executed like this:
+# > python accidents.py
+# then call the main function. However, if this file
+# was simply imported, then skip the call to main.
+if __name__ == "__main__":
+    main()
+
