97e7d8c56588cfc9c158c117921bc1ced67009e1
ruriona19 <roberto.uriona19@gmail.com>
2022-11-14 21:37:24 -0400

    Adding Error Handling to receipt

--- week10/prove_assignment/receipt.py
+++ week10/prove_assignment/receipt.py
@@ -17,54 +17,63 @@ QUANTITY_COLUMN = 1
 
 def main():
 
-    sales_tax_rate = 0.06 
-    number_of_items = 0
-    sub_total = 0.0
-    sales_tax = 0
-    current_date_and_time = datetime.now()
+    try:
+        sales_tax_rate = 0.06 
+        number_of_items = 0
+        sub_total = 0.0
+        sales_tax = 0
+        current_date_and_time = datetime.now()
 
-    # Prompt the user for a filename and open that text file.
-    products_dict = read_dict('products.csv', PRODUCT_NUMBER_COLUMN)
-    
-    print('Inkom Emporium')
-    print(products_dict)
+        # Prompt the user for a filename and open that text file.
+        products_dict = read_dict('products.csv', PRODUCT_NUMBER_COLUMN)
+        
+        print('Inkom Emporium')
+        #print(products_dict)
 
-    with open("request.csv", "rt") as request_file:
+        with open("request.csv", "rt") as request_file:
 
-        reader = csv.reader(request_file)
-        next(reader)
+            reader = csv.reader(request_file)
+            next(reader)
+
+            print()
+            print('Requested Items')
+
+            # Read each row in the CSV file one at a time.
+            # The reader object returns each row as a list.
+            for row_list in reader:
+
+                # For the current row, retrieve the
+                # values in columns 0, 3, and 4.
+                product_number_key = row_list[REQUEST_PRODUCT_NUMBER_COLUMN]
+                request_quantity = int(row_list[QUANTITY_COLUMN])
+                product_details = products_dict[product_number_key]
+                product_name = product_details[PRODUCT_NAME_COLUMN]
+                product_price = float(product_details[PRODUCT_PRICE_COLUMN])
+
+                number_of_items += request_quantity
+                sub_total += product_price*request_quantity
+
+                print(f'{product_name}: {request_quantity} @ {product_price}')
+            
+        sales_tax = sub_total*sales_tax_rate
+        total = sales_tax + sub_total
 
         print()
-        print('Requested Items')
-
-        # Read each row in the CSV file one at a time.
-        # The reader object returns each row as a list.
-        for row_list in reader:
-
-            # For the current row, retrieve the
-            # values in columns 0, 3, and 4.
-            product_number_key = row_list[REQUEST_PRODUCT_NUMBER_COLUMN]
-            request_quantity = int(row_list[QUANTITY_COLUMN])
-            product_details = products_dict[product_number_key]
-            product_name = product_details[PRODUCT_NAME_COLUMN]
-            product_price = float(product_details[PRODUCT_PRICE_COLUMN])
-
-            number_of_items += request_quantity
-            sub_total += product_price*request_quantity
-
-            print(f'{product_name}: {request_quantity} @ {product_price}')
+        print(f'Number of Items: {number_of_items}')
+        print(f'Subtotal: {sub_total:.2f}')
+        print(f'Sales Tax: {sales_tax:.2f}')
+        print(f'Total: {total:.2f}')   
+        print()
+        print('Thank you for shopping at the Inkom Emporium')   
+        print(f'{current_date_and_time:%a %b %d %I:%M:%S %Y}')
         
-    sales_tax = sub_total*sales_tax_rate
-    total = sales_tax + sub_total
-
-    print()
-    print(f'Number of Items: {number_of_items}')
-    print(f'Subtotal: {sub_total:.2f}')
-    print(f'Sales Tax: {sales_tax:.2f}')
-    print(f'Total: {total:.2f}')   
-    print()
-    print('Thank you for shopping at the Inkom Emporium')   
-    print(f'{current_date_and_time:%a %b %d %I:%M:%S %Y}') 
+    except FileNotFoundError as not_found_err:
+        print('Error: missing file')
+        print(not_found_err)
+    except PermissionError as perm_err:
+        print(perm_err)
+    except KeyError as key_err:
+        print(f'Error: unknown product ID in the request.csv file {key_err}' )    
 
 
 def read_dict(filename, key_column_index):
