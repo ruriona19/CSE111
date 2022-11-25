4413b09109ea3bc6b5f6c0907afd2eb52f937faa
ruriona19 <roberto.uriona19@gmail.com>
2022-11-25 15:39:30 -0400

    week11

--- week10/prove_assignment/receipt.py
+++ week10/prove_assignment/receipt.py
@@ -1,5 +1,9 @@
-# Import the csv module so that it can be used
-# to read from the accidents.csv file.
+"""
+File: receipt.py
+Author: Roberto Uriona
+Purpose: Write a Python program that handles exceptions, 
+including FileNotFoundError, PermissionError, and KeyError
+"""
 import csv
 from datetime import datetime
 
@@ -22,13 +26,16 @@ def main():
         number_of_items = 0
         sub_total = 0.0
         sales_tax = 0
+        # 8 Get the current date and time from your computer's operating 
+        # system and print the current date and time.
         current_date_and_time = datetime.now()
 
-        # Prompt the user for a filename and open that text file.
+        # Read the contents of a CSV file into a compound
+        # dictionary and return the dictionary.
         products_dict = read_dict('products.csv', PRODUCT_NUMBER_COLUMN)
         
-        print('Inkom Emporium')
-        #print(products_dict)
+        # 1 Print the store name at the top of the receipt.
+        print('Inkom Emporium')        
 
         with open("request.csv", "rt") as request_file:
 
@@ -42,31 +49,50 @@ def main():
             # The reader object returns each row as a list.
             for row_list in reader:
 
-                # For the current row, retrieve the
-                # values in columns 0, 3, and 4.
                 product_number_key = row_list[REQUEST_PRODUCT_NUMBER_COLUMN]
                 request_quantity = int(row_list[QUANTITY_COLUMN])
                 product_details = products_dict[product_number_key]
                 product_name = product_details[PRODUCT_NAME_COLUMN]
                 product_price = float(product_details[PRODUCT_PRICE_COLUMN])
 
+                # 3 Sum and print the number of ordered items.
                 number_of_items += request_quantity
+                # 4 Sum and print the subtotal due.
                 sub_total += product_price*request_quantity
 
+                # 2. Print the list of ordered items.
                 print(f'{product_name}: {request_quantity} @ {product_price}')
-            
+
+        # EXCEEDING THE REQUIREMENTS: Write code to discount the product prices by 10%
+        # if today is Tuesday or Wednesday.    
+        curernt_week_day_name = current_date_and_time.strftime('%A')
+
+        if curernt_week_day_name == 'Tuesday' or curernt_week_day_name == 'Wednesday':
+            sub_total = sub_total - sub_total*0.1 
+        
+        # 5 Compute and print the sales tax amount. Use 6% as the sales tax rate.
         sales_tax = sub_total*sales_tax_rate
+        # 6 Compute and print the total amount due.
         total = sales_tax + sub_total
 
         print()
+        # 3 Sum and print the number of ordered items.
         print(f'Number of Items: {number_of_items}')
+        # 4 Sum and print the subtotal due.
         print(f'Subtotal: {sub_total:.2f}')
+        # 5 Compute and print the sales tax amount. Use 6% as the sales tax rate.
         print(f'Sales Tax: {sales_tax:.2f}')
+        # 6 Compute and print the total amount due.
         print(f'Total: {total:.2f}')   
         print()
-        print('Thank you for shopping at the Inkom Emporium')   
+        # 7 Print a thank you message.
+        print('Thank you for shopping at the Inkom Emporium')  
+        # 8 Get the current date and time from your computer's operating 
+        # system and print the current date and time.
         print(f'{current_date_and_time:%a %b %d %I:%M:%S %Y}')
         
+    # 9 Include a try block and except blocks to handle FileNotFoundError, 
+    # PermissionError, and KeyError.        
     except FileNotFoundError as not_found_err:
         print('Error: missing file')
         print(not_found_err)
